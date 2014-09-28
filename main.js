9333bf66175fd436a12a457efc4d9d77b5059077
Yu Jiechao <fifth0725@gmail.com>
2014-09-28 21:11:02 +0800

    ver 2.0 with new ui & function

---
+++ main.js
@@ -0,0 +1,46 @@
+var backup;
+function random_senquence(){
+	len = prompt('enter the length of random DNA senquence:');
+	var v = new Array();
+	v[0]='A';
+	v[1]='T';
+	v[2]='G';
+	v[3]='C';
+	print='';
+	for (var i = 0; i < len; i++) {
+		print+=v[Math.floor(Math.random()*4)];
+	};
+	document.getElementById('senquence').value=print;
+}
+function send_quest(){
+	senquence=document.getElementById('senquence').value;
+	min_len=document.getElementById('min_len').value;
+	max_len=document.getElementById('max_len').value;
+	repeat=document.getElementById('repeat').value;
+	r=document.getElementById('r').value;
+	var xmlhttp;
+	xmlhttp=new XMLHttpRequest();
+	xmlhttp.onreadystatechange=function(){
+		if (xmlhttp.readyState==4 && xmlhttp.status==200){
+			backup=xmlhttp.responseText
+			var list=eval('('+backup+')');
+			answer='senquence|repeat|length<br />';
+			for (key in list) {
+				answer+=list[key]['senquence'];
+				answer+="|";
+				answer+=list[key]['repeat'];
+				answer+="|";
+				answer+=list[key]['length'];
+				answer+="<br />";
+			}
+			document.getElementById("answer").innerHTML=answer;
+		}
+	}	
+	xmlhttp.open("POST","msatr.php",true);
+	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xmlhttp.send("senquence="+senquence+"&min_len="+min_len+"&max_len="+max_len+"&repeat="+repeat+"&r="+r);
+}
+function scroll_to(step){
+	a=new Array("input", "param", "output", "answer");
+	$.scrollTo('#step_'+a[step], 500);
+}
